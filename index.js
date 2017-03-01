0f9e1f211953c5a01e73ea761e65d456cf0a6012
sunya9 <mugen.xyz@gmail.com>
2017-03-01 19:57:59 +0900

    Add node-cron

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Cafe = require('./lib/Cafe')
 const debug = require('debug')('cafemenu2twitter')
+const cron = require('node-cron')
 require('dotenv').config()
 
 const {
@@ -15,16 +16,17 @@ const times = {
   afternoon: 2,
   dinner: 3
 }
-
-Promise.all(process.argv.slice(2).map(n => new Cafe(times.afternoon, n).get()))
-  .catch(debug)
-  .then(getTemplates)
-  .catch(debug)
-  .then(captureHtmls)
-  .catch(debug)
-  .then(trimmingCaptures)
-  .catch(debug)
-  .then(uploadScreenshots)
-  .catch(debug)
-  .then(post2twitter)
-  .catch(debug)
+cron.schedule('0 11 * * 1-6', () => {
+  Promise.all('1234'.split('').map(n => new Cafe(times.afternoon, n).get()))
+    .catch(debug)
+    .then(getTemplates)
+    .catch(debug)
+    .then(captureHtmls)
+    .catch(debug)
+    .then(trimmingCaptures)
+    .catch(debug)
+    .then(uploadScreenshots)
+    .catch(debug)
+    .then(post2twitter)
+    .catch(debug)
+})
