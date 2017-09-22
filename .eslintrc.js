3df1d83213d4e122018824b77112991deb20dc86
sunya9 <mugen.xyz@gmail.com>
2017-09-23 07:14:01 +0900

    Fix eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,8 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": "eslint:recommended",
     "parserOptions": {
-        "sourceType": "module"
+      "ecmaVersion": 2017
     },
     "rules": {
         "indent": [
