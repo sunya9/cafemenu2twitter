cb003b345d3958961319161ac6dabaeed17e8d99
sunya9 <mugen.xyz@gmail.com>
2017-09-24 18:17:30 +0900

    Simplify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,15 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "parserOptions": {
-      "ecmaVersion": 2017
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
-};
+  env: {
+    es6: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"]
+  }
+}
